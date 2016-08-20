4ca923ede9b9429b401b9ebb252a31f30ec6ba7f
Kevin M. Godby <kevin@godby.org>
2016-08-19 23:26:38 -0500

    All that work for constexpr and I'm not using those vars anyway. Oh, well.

--- test/Display/test_Orientation.cpp
+++ test/Display/test_Orientation.cpp
@@ -37,10 +37,6 @@ using R = ::osvr::display::Rotation;
 using O = ::osvr::display::DesktopOrientation;
 using SO = ::osvr::display::ScanOutOrigin;
 
-OSVR_DISPLAY_CONSTEXPR auto Rotations = { R::Zero, R::Ninety, R::OneEighty, R::TwoSeventy };
-OSVR_DISPLAY_CONSTEXPR auto Orientations = { O::Landscape, O::Portrait, O::LandscapeFlipped, O::PortraitFlipped };
-OSVR_DISPLAY_CONSTEXPR auto ScanOutOrigins = { SO::UpperLeft, SO::UpperRight, SO::LowerRight, SO::LowerLeft };
-
 TEST(DifferenceBetweenOrientations, Identity)
 {
     EXPECT_EQ(R::Zero, O::Landscape - O::Landscape);
